f8bb4ef6fbd1798de570c704788ea54ab260fbd0
Jon Kaplan <jkaplan621@gmail.com>
2024-02-15 20:50:19 -0500

    Revert "Updated deploy for pnpm"
    This reverts commit d78ff0e24d2238e871b2469b223965f4423f06a3.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jtk10001.github.io',
+  site: 'https://astrofy-template.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()]
 });
