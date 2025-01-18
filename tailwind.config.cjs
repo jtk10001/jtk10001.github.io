0a93c92e20cb7a3aa192e9026414e58c4f1d3ccd
jtk10001 <jkaplan621@gmail.com>
2025-01-18 13:58:07 -0500

    Home page rework, inclusion of pdf resume

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,42 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
+		colors: {
+		  'primary-content': 'hsl(var(--pc))',
+		  'secondary-content': 'hsl(var(--sc))',
+		},
+		typography: {
+		  DEFAULT: {
+			css: {
+			  maxWidth: '100ch',
+			},
+		  },
+		},
+	  },
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
+	plugins: [
+	  require("@tailwindcss/typography"),
+	  require("daisyui")
+	],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
-}
+	  themes: [{
+		modern: {
+		  "primary": "#2A3036",
+		  "primary-content": "#ffffff",
+		  "secondary": "#4B5563",
+		  "accent": "#374151",
+		  "neutral": "#1F2937",
+		  "base-100": "#ffffff",
+		  "base-200": "#F9FAFB",
+		  "base-300": "#F3F4F6",
+		  "info": "#3B82F6",
+		  "success": "#10B981",
+		  "warning": "#F59E0B",
+		  "error": "#EF4444",
+		}
+	  }, "light", "dark"],
+	  darkTheme: "dark",
+	  logs: false,
+	}
+  }
